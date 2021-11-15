0a0aa849617c815584276391e48b9f523b51a1c4
espositow <espositow@gmail.com>
2021-11-15 18:17:36 -0300

    Controller final

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -23,41 +23,41 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
-//@AllArgsConstructor(onConstructor = @__(@Autowired))
-public class PersonController {
-    @GetMapping
-    public String getBook(){
-        return "Hevellyn e Linha Amo vocês!";
-    }
-}
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 //public class PersonController {
-//
-//    private PersonService personService;
-//
-//    @PostMapping
-//    @ResponseStatus(HttpStatus.CREATED)
-//    public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
-//        return personService.createPerson(personDTO);
-//    }
-//
 //    @GetMapping
-//    public List<PersonDTO> listAll() {
-//        return personService.listAll();
-//    }
-//
-//    @GetMapping("/{id}")
-//    public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
-//        return personService.findById(id);
-//    }
-//
-//    @PutMapping("/{id}")
-//    public MessageResponseDTO updateById(@PathVariable Long id, @RequestBody @Valid PersonDTO personDTO) throws PersonNotFoundException {
-//        return personService.updateById(id, personDTO);
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    @ResponseStatus(HttpStatus.NO_CONTENT)
-//    public void deleteById(@PathVariable Long id) throws PersonNotFoundException {
-//        personService.delete(id);
+//    public String getBook(){
+//        return "API test!";
 //    }
 //}
+public class PersonController {
+
+    private PersonService personService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
+        return personService.createPerson(personDTO);
+    }
+
+    @GetMapping
+    public List<PersonDTO> listAll() {
+        return personService.listAll();
+    }
+
+    @GetMapping("/{id}")
+    public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
+        return personService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public MessageResponseDTO updateById(@PathVariable Long id, @RequestBody @Valid PersonDTO personDTO) throws PersonNotFoundException {
+        return personService.updateById(id, personDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) throws PersonNotFoundException {
+        personService.delete(id);
+    }
+}
